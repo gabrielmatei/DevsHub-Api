e7b8fe66f57efce2862f0bedd9c8db6763a37571
Gabriel Matei <gabrielvmatei@gmail.com>
2020-11-09 19:37:42 +0200

    Values CRUD

--- DevsHub/Controllers/V1/ValuesController.cs
+++ DevsHub/Controllers/V1/ValuesController.cs
@@ -1,6 +1,9 @@
 ﻿using DevsHub.Contracts.V1;
+using DevsHub.Domain;
 using DevsHub.Services;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace DevsHub.Controllers.V1
@@ -20,10 +23,59 @@ namespace DevsHub.Controllers.V1
         /// </summary>
         /// <response code="200">Returns all values</response>
         [HttpGet(ApiRoutes.Values.GetAll)]
+        [ProducesResponseType(typeof(List<Value>), 200)]
         public async Task<IActionResult> GetAll()
         {
             var values = await _valueService.GetValuesAsync();
             return Ok(values);
+        }
+
+        [HttpGet(ApiRoutes.Values.Get)]
+        [ProducesResponseType(typeof(Value), 200)]
+        public async Task<IActionResult> GetAll([FromRoute] Guid id)
+        {
+            var value = await _valueService.GetValueByIdAsync(id);
+            if (value == null)
+                return NotFound();
+            return Ok(value);
+        }
+
+        [HttpPost(ApiRoutes.Values.Create)]
+        public async Task<IActionResult> Create([FromBody] Value request)
+        {
+            var value = new Value
+            {
+                Name = request.Name
+            };
+
+            await _valueService.CreateValueAsync(value);
+
+            var locationUri = $"{HttpContext.Request.Scheme}://{HttpContext.Request.Host.ToUriComponent()}/{ApiRoutes.Values.Get.Replace("{id}", value.Id.ToString())}";
+
+            var response = new Value { Id = value.Id };
+
+            return Created(locationUri, response);
+        }
+
+        [HttpPut(ApiRoutes.Values.Update)]
+        public async Task<IActionResult> Update([FromRoute] Guid id, [FromBody] Value request)
+        {
+            var value = await _valueService.GetValueByIdAsync(id);
+            value.Name = request.Name;
+
+            var updated = await _valueService.UpdateValueAsync(value);
+            if (updated)
+                return Ok(value);
+            return NotFound();
+        }
+
+        [HttpDelete(ApiRoutes.Values.Delete)]
+        public async Task<IActionResult> Delete([FromRoute] Guid id)
+        {
+            var deleted = await _valueService.DeleteValueAsync(id);
+            if (deleted)
+                return NoContent();
+            return NotFound();
         }
     }
 }
--- DevsHub/Services/ValueService.cs
+++ DevsHub/Services/ValueService.cs
@@ -1,6 +1,7 @@
 ﻿using DevsHub.Data;
 using DevsHub.Domain;
 using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -9,6 +10,10 @@ namespace DevsHub.Services
     public interface IValueService
     {
         Task<List<Value>> GetValuesAsync();
+        Task<Value> GetValueByIdAsync(Guid id);
+        Task<bool> CreateValueAsync(Value value);
+        Task<bool> UpdateValueAsync(Value value);
+        Task<bool> DeleteValueAsync(Guid id);
     }
 
     public class ValueService : IValueService
@@ -23,6 +28,36 @@ namespace DevsHub.Services
         public async Task<List<Value>> GetValuesAsync()
         {
             return await _dataContext.Values.ToListAsync();
+        }
+
+        public async Task<Value> GetValueByIdAsync(Guid id)
+        {
+            return await _dataContext.Values.SingleOrDefaultAsync(value => value.Id == id);
+        }
+
+        public async Task<bool> CreateValueAsync(Value value)
+        {
+            await _dataContext.Values.AddAsync(value);
+            var created = await _dataContext.SaveChangesAsync();
+            return created > 0;
+        }
+
+        public async Task<bool> UpdateValueAsync(Value value)
+        {
+            _dataContext.Values.Update(value);
+            var updated = await _dataContext.SaveChangesAsync();
+            return updated > 0;
+        }
+
+        public async Task<bool> DeleteValueAsync(Guid id)
+        {
+            var value = await GetValueByIdAsync(id);
+            if (value == null)
+                return false;
+
+            _dataContext.Values.Remove(value);
+            var deleted = await _dataContext.SaveChangesAsync();
+            return deleted > 0;
         }
     }
 }
